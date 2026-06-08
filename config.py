b7af0a55ef1f04326578bffc9730c803eff83fe8
KWFD306 <felicia.kamdom@2029.ucac-icam.com>
2026-06-08 14:19:44 +0100

    Passage à PostgreSQL

--- config.py
+++ config.py
@@ -2,5 +2,14 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'votre_cle_secrete_super_secure'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///usv_drone.db'
+    
+    database_url = os.environ.get('DATABASE_URL')
+    
+    if database_url:
+        if database_url.startswith("postgres://"):
+            database_url = database_url.replace("postgres://", "postgresql://", 1)
+        SQLALCHEMY_DATABASE_URI = database_url
+    else:
+        SQLALCHEMY_DATABASE_URI = 'sqlite:///usv_drone.db'
+        
     SQLALCHEMY_TRACK_MODIFICATIONS = False
